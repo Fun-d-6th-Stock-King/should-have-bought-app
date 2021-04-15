4763800c5c4fec650b705b5a3df82b84d81980ba
임수빈 <sbim@cafe24corp.com>
2021-04-15 19:17:37 +0900

    검색 구현 완료

--- lib/providers/calculator/calculator_provider.dart
+++ lib/providers/calculator/calculator_provider.dart
@@ -4,10 +4,37 @@ import 'package:should_have_bought_app/models/calculator/company.dart';
 
 class CalculatorProvider with ChangeNotifier {
   List _companyList = [];
+  List _searchCompanyList = [];
 
   List get companyList {
     return _companyList;
   }
+  List get searchCompanyList {
+    return _searchCompanyList;
+  }
+
+  void filterSearchResults(String query) {
+    List<Company> dummySearchList = [];
+    dummySearchList.addAll(_companyList?.cast<Company>());
+    print('dummySearchList:');
+    if(query.isNotEmpty) {
+      List<Company> dummyListData = [];
+      dummyListData = dummySearchList.where((item) {
+       return item.company.contains(query);
+      }).toList();
+
+      _searchCompanyList.clear();
+      _searchCompanyList.addAll(dummyListData);
+      print(dummyListData);
+      notifyListeners();
+      return;
+    } else {
+      _searchCompanyList.clear();
+      _searchCompanyList.addAll(_companyList);
+      notifyListeners();
+    }
+  }
+
   Future getResult() async {
     final result = await CalculatorApi.getResult();
     // print('[getarticles]');
@@ -26,6 +53,8 @@ class CalculatorProvider with ChangeNotifier {
     _companyList = list.map((company) =>
         Company(company['company'],company['code'])
     ).toList();
+    _searchCompanyList = [..._companyList];
+
     print(_companyList[1].code);
     notifyListeners();
   }
--- lib/widgets/calculator/calculator_widget.dart
+++ lib/widgets/calculator/calculator_widget.dart
@@ -14,10 +14,10 @@ class CalculatorWidget extends StatefulWidget {
 }
 
 class _CalculatorWidgetState extends State<CalculatorWidget> {
-  final TextEditingController _controller = new TextEditingController();
-
+  final TextEditingController _controller = TextEditingController();
+  TextEditingController _searchController = TextEditingController();
   String _selectedDateValue = '10년 전';
-  Company _selectedCompany = Company('삼성전자','005930');
+  Company _selectedCompany = Company('삼성전자', '005930');
   int _selectedPriceValue = 100000;
   int value = 0;
   List<String> dates = ['어제', '저번주', '한달 전', '6개월 전', '1년 전', '5년 전', '10년 전'];
@@ -30,7 +30,19 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
     'LG 전자',
     'LG 디스플레이'
   ];
+  Future futureGetCompanyList;
 
+  @override
+  void initState() {
+    super.initState();
+    futureGetCompanyList = Provider.of<CalculatorProvider>(context, listen: false)
+        .getCompanies();
+  }
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+  }
   void SetCompanyValue(Company company) {
     setState(() {
       _selectedCompany = company;
@@ -99,7 +111,8 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
           ],
         ),
         child: Padding(
-          padding: const EdgeInsets.only(left: 20,right:20, top:12,bottom: 12),
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 12, bottom: 12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -113,7 +126,7 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
               Row(
                 children: [
                   SizedBox(
-                    height : 50,
+                    height: 50,
                     child: Text(
                       '샀었더라면..?',
                       textAlign: TextAlign.start,
@@ -126,27 +139,27 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
                   ),
                 ],
               ),
-          SizedBox(
-            height: 50,
-          ),
-          Container(
-            height: 50,
-            width: MediaQuery.of(context).size.width,
-            child: CupertinoButton(
-              color: mainColor,
-              padding:EdgeInsets.all(0),
-              child: Text(
-                '지금 얼마?',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Color(0xFFFFFFFF),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                ),
+              SizedBox(
+                height: 50,
               ),
-              onPressed: () {},
-            ),
-          )
+              Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: CupertinoButton(
+                  color: mainColor,
+                  padding: EdgeInsets.all(0),
+                  child: Text(
+                    '지금 얼마?',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              )
             ],
           ),
         ),
@@ -157,8 +170,10 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
   Widget viewSelectedDates(BuildContext context) {
     return Row(
       children: [
-        SelectedButton(context,_selectedDateValue, _showDatePicker),
-        Padding(padding:EdgeInsets.only(right:10),),
+        SelectedButton(context, _selectedDateValue, _showDatePicker),
+        Padding(
+          padding: EdgeInsets.only(right: 10),
+        ),
         Text(
           '에',
           style: TextStyle(
@@ -173,8 +188,10 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
   Widget viewSelectedCompany(BuildContext context) {
     return Row(
       children: [
-        SelectedButton(context,_selectedCompany.company, showCompaniesMenu),
-        Padding(padding:EdgeInsets.only(right:10),),
+        SelectedButton(context, _selectedCompany.company, showCompaniesMenu),
+        Padding(
+          padding: EdgeInsets.only(right: 10),
+        ),
         Text(
           '를',
           style: TextStyle(
@@ -185,18 +202,18 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
       ],
     );
   }
-  Widget SelectedButton(BuildContext context, String value, ValueChanged<BuildContext> showMenu) {
+
+  Widget SelectedButton(
+      BuildContext context, String value, ValueChanged<BuildContext> showMenu) {
     return Container(
       height: 45,
       decoration: BoxDecoration(
-        border: Border(
-            bottom: BorderSide(
-              color: Colors.grey,
-            )
-        )
-      ),
+          border: Border(
+              bottom: BorderSide(
+        color: Colors.grey,
+      ))),
       child: CupertinoButton(
-        padding:EdgeInsets.all(0),
+        padding: EdgeInsets.all(0),
         child: Text(
           value,
           textAlign: TextAlign.left,
@@ -214,79 +231,120 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
   Widget viewSelectedPrice(BuildContext context) {
     return Row(
       children: [
-          ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: 300,
-            ),
-            child: IntrinsicWidth(
-              child: TextField(
-                textAlign: TextAlign.left,
-                maxLength: 10,
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  WhitelistingTextInputFormatter(RegExp('[0-9]')),
-                ],
-                style: TextStyle(
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 300,
+          ),
+          child: IntrinsicWidth(
+            child: TextField(
+              textAlign: TextAlign.left,
+              maxLength: 10,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                WhitelistingTextInputFormatter(RegExp('[0-9]')),
+              ],
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+              ),
+              cursorColor: Colors.grey,
+              controller: _controller,
+              decoration: InputDecoration(
+                isDense: true,
+                hintText: '100000',
+                hintStyle: TextStyle(
                   color: Colors.black,
                   fontSize: 28,
                   fontWeight: FontWeight.w600,
                 ),
-                cursorColor: Colors.grey,
-                controller: _controller,
-                decoration: InputDecoration(
-                  isDense: true,
-                  hintText: '100000',
-                  hintStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  counterText: '',
-                ),
+                counterText: '',
               ),
             ),
           ),
-        Padding(padding:EdgeInsets.only(right:10),),
-         Text(
-            '원에',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w100,
-            ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 10),
+        ),
+        Text(
+          '원에',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w100,
           ),
+        ),
       ],
     );
   }
 
-
-  void showCompaniesMenu(BuildContext context) {
+  void showCompaniesMenu(BuildContext context) async {
     showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(8.0),
-          topRight: Radius.circular(8.0),
-        )),
-        builder: (ctx) {
-          return CompanyListBuilder(context);
-        });
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(8.0),
+        topRight: Radius.circular(8.0),
+      )),
+      builder: (ctx) {
+        return CompanyListBuilder(context);
+      }
+    ).whenComplete(() {
+      //SystemChannels.textInput.invokeMethod('TextInput.hide');
+    });
   }
 
   Widget CompanyListBuilder(BuildContext context) {
     return FutureBuilder(
-        future: Provider.of<CalculatorProvider>(context, listen: false)
-            .getCompanies(),
+        future: futureGetCompanyList,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Scaffold(
-                backgroundColor: Colors.transparent,
-                body: Center(
-                  child: CircularProgressIndicator(
-                      backgroundColor: Colors.transparent
+            return Container(
+              height: 300,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: 400,
+                  maxHeight: 400,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 15),
+                      SizedBox(
+                        height: 40,
+                        child: Material(
+                          elevation: 5.0,
+                          shadowColor: Colors.black,
+                          child: TextField(
+                              onChanged: (value) {
+                              },
+                              controller: _searchController,
+                              decoration: InputDecoration(
+                                  hintText: "Search",
+                                  suffixIcon: Icon(Icons.search),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  contentPadding:
+                                  EdgeInsets.fromLTRB(10.0, 10.0, 20.0, 10.0),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      borderSide: BorderSide(
+                                          color: Colors.white, width: 3.0)))),
+                        ),
+                      ),
+                      SizedBox(height: 55),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 30),
+                        child: CircularProgressIndicator(
+                                  backgroundColor: Colors.transparent),
+                      ),
+                    ],
                   ),
-                ));
+                ),
+              ),
+            );
           }
           if (snapshot.connectionState == ConnectionState.done) {
             return CompanyList(context);
@@ -301,54 +359,96 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
   Widget CompanyList(BuildContext context) {
     return Consumer<CalculatorProvider>(
         child: emptyCompanyList(context),
-        builder: (context, calculatorProvider, child) =>
-            calculatorProvider.companyList.isEmpty == true
-                ? child
-                : ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minHeight: 400,
-                      maxHeight: 400,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left:34.0,right: 34.0),
-                      child: Column(
-                        children: [
-                          InkWell(
-                            onTap: () {},
-                            child: ListTile(
-                              title: Align(
-                                child: Text('검색'),
-                                alignment: Alignment(-0.83, 0),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: ListView.builder(
-                                shrinkWrap: true,
-                                scrollDirection: Axis.vertical,
-                                itemCount: calculatorProvider.companyList.length,
-                                itemBuilder: (context, index) {
-                                  return CompanyItem(
-                                      company:calculatorProvider.companyList[index],
-                                      onTap: SetCompanyValue,
-                                  );
-                                }),
-                          ),
-                        ],
+        builder: (context, calculatorProvider, child) => ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: 400,
+                maxHeight: 400,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: 15),
+                    SizedBox(
+                      height: 40,
+                      child: Material(
+                        elevation: 5.0,
+                        shadowColor: Colors.black,
+                        child: TextField(
+                            onChanged: (value) {
+                              calculatorProvider.filterSearchResults(value);
+                            },
+                            controller: _searchController,
+                            decoration: InputDecoration(
+                                hintText: "Search",
+                                suffixIcon: Icon(Icons.search),
+                                fillColor: Colors.white,
+                                filled: true,
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(10.0, 10.0, 20.0, 10.0),
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                    borderSide: BorderSide(
+                                        color: Colors.white, width: 3.0)))),
                       ),
                     ),
-                  ));
+                    SizedBox(height: 15),
+                    Expanded(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.vertical,
+                          itemCount:
+                              calculatorProvider.searchCompanyList.length,
+                          itemBuilder: (context, index) {
+                            return CompanyItem(
+                              company:
+                                  calculatorProvider.searchCompanyList[index],
+                              onTap: SetCompanyValue,
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+            ));
   }
 
   Widget emptyCompanyList(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.pop(context);
-      },
-      child:ListTile(
-        leading: Text('leading'),
-        title:Text('title'),
-
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minHeight: 400,
+        maxHeight: 400,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            SizedBox(
+              height: 40,
+              child: Material(
+                elevation: 5.0,
+                shadowColor: Colors.black,
+                child: TextField(
+                    onChanged: (value) {
+                    },
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                        hintText: "Search",
+                        suffixIcon: Icon(Icons.search),
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding:
+                        EdgeInsets.fromLTRB(10.0, 10.0, 20.0, 10.0),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                                color: Colors.white, width: 3.0)))),
+              ),
+            ),
+            SizedBox(height: 15),
+          ],
+        ),
       ),
     );
   }
