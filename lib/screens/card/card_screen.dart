0c64efd0989eebc0c6b4d331d9a5acff61aca8f4
임수빈 <sbim@cafe24corp.com>
2021-04-22 16:46:52 +0900

    swiped 라이브러리 커스텀

--- lib/screens/card/card_screen.dart
+++ lib/screens/card/card_screen.dart
@@ -1,13 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:should_have_bought_app/widgets/card/lib/swipe_cards.dart';
 
-class CardScreen extends StatelessWidget {
+
+class CardScreen extends StatefulWidget  {
+  @override
+  _CreateCardScreen createState() => _CreateCardScreen();
+}
+class _CreateCardScreen extends State<CardScreen> {
+
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  List<SwipeItem> _swipeItems = List<SwipeItem>();
+  MatchEngine _matchEngine;
+  List<String> _names = ["Red", "Blue", "Green", "Yellow", "Orange"];
+  List<Color> _colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+    Colors.orange
+  ];
 
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(
-        child: Text('CardScreen'),
-      ),
-    );
+  void initState() {
+    for (int i = 0; i < _names.length; i++) {
+      _swipeItems.add(SwipeItem(
+          content: Content(text: _names[i], color: _colors[i]),
+          likeAction: () {
+            _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text("Liked ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          },
+          nopeAction: () {
+            _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text("Nope ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          },
+          superlikeAction: () {
+            _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text("Superliked ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          }));
+    }
+
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    super.initState();
   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: _scaffoldKey,
+        body: Container(
+            child: Column(children: [
+              Container(
+                height: 300,
+                width: 300,
+                child: SwipeCards(
+                  matchEngine: _matchEngine,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      color: _swipeItems[index].content.color,
+                      child: Text(
+                        _swipeItems[index].content.text,
+                        style: TextStyle(fontSize: 100),
+                      ),
+                    );
+                  },
+                  onStackFinished: () {
+                    _scaffoldKey.currentState.showSnackBar(SnackBar(
+                      content: Text("Stack Finished"),
+                      duration: Duration(milliseconds: 500),
+                    ));
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton(
+                      onPressed: () {
+                        _matchEngine.currentItem.nope();
+                      },
+                      child: Text("Nope")),
+                  RaisedButton(
+                      onPressed: () {
+                        _matchEngine.currentItem.superLike();
+                      },
+                      child: Text("Superlike")),
+                  RaisedButton(
+                      onPressed: () {
+                        _matchEngine.currentItem.like();
+                      },
+                      child: Text("Like"))
+                ],
+              )
+            ])));
+  }
+}
+
+class Content {
+  final String text;
+  final Color color;
+
+  Content({this.text, this.color});
 }
---
+++ lib/widgets/card/lib/swipe_cards.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/material.dart';
+import 'package:should_have_bought_app/widgets/card/lib/draggable_card.dart';
+import 'package:should_have_bought_app/widgets/card/lib/profile_card.dart';
+
+
+class SwipeCards extends StatefulWidget {
+  final IndexedWidgetBuilder itemBuilder;
+  final MatchEngine matchEngine;
+  final Function onStackFinished;
+
+  const SwipeCards(
+      {Key key, this.matchEngine, this.onStackFinished, this.itemBuilder})
+      : super(key: key);
+
+  @override
+  _SwipeCardsState createState() => _SwipeCardsState();
+}
+
+class _SwipeCardsState extends State<SwipeCards> {
+  Key _frontCard;
+  SwipeItem _currentItem;
+  double _nextCardScale = 0.9;
+  SlideRegion slideRegion;
+
+  @override
+  void initState() {
+    widget.matchEngine.addListener(_onMatchEngineChange);
+    _currentItem = widget.matchEngine.currentItem;
+    _currentItem.addListener(_onMatchChange);
+    _frontCard = Key(widget.matchEngine._currentItemIndex.toString());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (_currentItem != null) {
+      _currentItem.removeListener(_onMatchChange);
+    }
+    widget.matchEngine.removeListener(_onMatchEngineChange);
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(SwipeCards oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.matchEngine != oldWidget.matchEngine) {
+      oldWidget.matchEngine.removeListener(_onMatchEngineChange);
+      widget.matchEngine.addListener(_onMatchEngineChange);
+    }
+    if (_currentItem != null) {
+      _currentItem.removeListener(_onMatchChange);
+    }
+    _currentItem = widget.matchEngine.currentItem;
+    if (_currentItem != null) {
+      _currentItem.addListener(_onMatchChange);
+    }
+  }
+
+  void _onMatchEngineChange() {
+    setState(() {
+      if (_currentItem != null) {
+        _currentItem.removeListener(_onMatchChange);
+      }
+      _currentItem = widget.matchEngine.currentItem;
+      if (_currentItem != null) {
+        _currentItem.addListener(_onMatchChange);
+      }
+      _frontCard = Key(widget.matchEngine._currentItemIndex.toString());
+    });
+  }
+
+  void _onMatchChange() {
+    setState(() {
+      //match has been changed
+    });
+  }
+
+  Widget _buildFrontCard() {
+    return ProfileCard(
+      child: widget.itemBuilder(context, widget.matchEngine._currentItemIndex),
+      key: _frontCard,
+    );
+  }
+
+  Widget _buildBackCard() {
+    return Transform(
+      transform: Matrix4.identity()..scale(_nextCardScale, _nextCardScale),
+      alignment: Alignment.center,
+      child: ProfileCard(
+        child: widget.itemBuilder(context, widget.matchEngine._nextItemIndex),
+      ),
+    );
+  }
+
+  void _onSlideUpdate(double distance) {
+    setState(() {
+      _nextCardScale = 0.9 + (0.1 * (distance / 100.0)).clamp(0.0, 0.1);
+    });
+  }
+
+  void _onSlideRegion(SlideRegion region) {
+    setState(() {
+      slideRegion = region;
+    });
+  }
+
+  void _onSlideOutComplete(SlideDirection direction) {
+    SwipeItem currentMatch = widget.matchEngine.currentItem;
+    switch (direction) {
+      case SlideDirection.left:
+        currentMatch.nope();
+        break;
+      case SlideDirection.right:
+        currentMatch.like();
+        break;
+      case SlideDirection.up:
+        currentMatch.superLike();
+        break;
+    }
+
+    widget.matchEngine.cycleMatch();
+    if (widget.matchEngine.currentItem == null) {
+      widget.onStackFinished();
+    }
+  }
+
+  SlideDirection _desiredSlideOutDirection() {
+    switch (widget.matchEngine.currentItem.decision) {
+      case Decision.nope:
+        return SlideDirection.left;
+      case Decision.like:
+        return SlideDirection.right;
+      case Decision.superLike:
+        return SlideDirection.up;
+      default:
+        return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        if (widget.matchEngine.nextItem != null)
+          DraggableCard(
+            isDraggable: false,
+            card: _buildBackCard(),
+          ),
+        if (widget.matchEngine.currentItem != null)
+          DraggableCard(
+            card: _buildFrontCard(),
+            slideTo: _desiredSlideOutDirection(),
+            onSlideUpdate: _onSlideUpdate,
+            onSlideRegionUpdate: _onSlideRegion,
+            onSlideOutComplete: _onSlideOutComplete,
+          )
+      ],
+    );
+  }
+}
+
+class MatchEngine extends ChangeNotifier {
+  final List<SwipeItem> _swipeItems;
+  int _currentItemIndex;
+  int _nextItemIndex;
+
+  MatchEngine({
+    List<SwipeItem> swipeItems,
+  }) : _swipeItems = swipeItems {
+    _currentItemIndex = 0;
+    _nextItemIndex = 1;
+  }
+
+  SwipeItem get currentItem => _currentItemIndex < _swipeItems.length
+      ? _swipeItems[_currentItemIndex]
+      : null;
+
+  SwipeItem get nextItem =>
+      _nextItemIndex < _swipeItems.length ? _swipeItems[_nextItemIndex] : null;
+
+  void cycleMatch() {
+    if (currentItem.decision != Decision.undecided) {
+      currentItem.resetMatch();
+      _currentItemIndex = _nextItemIndex;
+      _nextItemIndex = _nextItemIndex + 1;
+      notifyListeners();
+    }
+  }
+
+  void rewindMatch() {
+    if (_currentItemIndex != 0) {
+      currentItem.resetMatch();
+      _nextItemIndex = _currentItemIndex;
+      _currentItemIndex = _currentItemIndex - 1;
+      currentItem.resetMatch();
+      notifyListeners();
+    }
+  }
+}
+
+class SwipeItem extends ChangeNotifier {
+  final dynamic content;
+  final Function likeAction;
+  final Function superlikeAction;
+  final Function nopeAction;
+  Decision decision = Decision.undecided;
+
+  SwipeItem(
+      {this.content, this.likeAction, this.superlikeAction, this.nopeAction});
+
+  void like() {
+    if (decision == Decision.undecided) {
+      decision = Decision.like;
+      try {
+        likeAction();
+      } catch (e) {}
+      notifyListeners();
+    }
+  }
+
+  void nope() {
+    if (decision == Decision.undecided) {
+      decision = Decision.nope;
+      try {
+        nopeAction();
+      } catch (e) {}
+      notifyListeners();
+    }
+  }
+
+  void superLike() {
+    if (decision == Decision.undecided) {
+      decision = Decision.superLike;
+      try {
+        superlikeAction();
+      } catch (e) {}
+      notifyListeners();
+    }
+  }
+
+  void resetMatch() {
+    if (decision != Decision.undecided) {
+      decision = Decision.undecided;
+      notifyListeners();
+    }
+  }
+}
+
+enum Decision { undecided, nope, like, superLike }
