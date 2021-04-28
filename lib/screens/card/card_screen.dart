6590899f528aaf4fdac8e1ebd97fefb78e95b248
Soobinnn <isb9082@naver.com>
2021-04-28 18:59:36 +0900

    format 추가

--- lib/screens/card/card_screen.dart
+++ lib/screens/card/card_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:should_have_bought_app/providers/buy_or_not/buy_or_not_provider.dart';
 import 'package:should_have_bought_app/widgets/card/lib/swipe_cards.dart';
+import 'package:should_have_bought_app/widgets/chart/chart_wiget.dart';
 
 class CardScreen extends StatefulWidget {
   @override
@@ -12,7 +13,7 @@ class _CreateCardScreen extends State<CardScreen> {
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   List<SwipeItem> _swipeItems = List<SwipeItem>();
   MatchEngine _matchEngine;
-  List<String> _names = ["Red", "Blue", "Green", "Yellow", "Orange"];
+  List<String> _names = ["삼성전자", "SK 하이닉스", "카카오", "현대자동차", "네이버"];
   List<Color> _colors = [
     Colors.red,
     Colors.blue,
@@ -27,26 +28,28 @@ class _CreateCardScreen extends State<CardScreen> {
       _swipeItems.add(SwipeItem(
           content: Content(text: _names[i], color: _colors[i]),
           likeAction: () {
-            print('like');
-            _scaffoldKey.currentState.showSnackBar(SnackBar(
-              content: Text("Liked ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
+            print('LEFT');
+            // _scaffoldKey.currentState.showSnackBar(SnackBar(
+            //   content: Text("Liked ${_names[i]}"),
+            //   duration: Duration(milliseconds: 500),
+            // ));
           },
           nopeAction: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text("Nope ${_names[i]}"),
-                  duration: Duration(milliseconds: 500),
-                )
-            );
+            print('UP');
+            // ScaffoldMessenger.of(context).showSnackBar(
+            //     SnackBar(
+            //       content: Text("Nope ${_names[i]}"),
+            //       duration: Duration(milliseconds: 500),
+            //     )
+            // );
             // _scaffoldKey.currentState.showSnackBar();
           },
           superlikeAction: () {
-            _scaffoldKey.currentState.showSnackBar(SnackBar(
-              content: Text("Superliked ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
+            print('RIGHT');
+            // _scaffoldKey.currentState.showSnackBar(SnackBar(
+            //   content: Text("Superliked ${_names[i]}"),
+            //   duration: Duration(milliseconds: 500),
+            // ));
           }));
     }
 
@@ -57,7 +60,7 @@ class _CreateCardScreen extends State<CardScreen> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    Provider.of<BuyOrNotProvider>(context, listen: false).getEvaluationLists();
+    //Provider.of<BuyOrNotProvider>(context, listen: false).getEvaluationLists();
   }
 
   @override
@@ -133,13 +136,17 @@ class _CreateCardScreen extends State<CardScreen> {
                     //     'https://www.woolha.com/media/2020/03/eevee.png'),
                     backgroundColor: Colors.grey,
                   ),
-                  Text('삼성전자'),
+                  Padding(padding: EdgeInsets.only(left: 7)),
+                  Text(_swipeItems[index].content.text, style:TextStyle(fontSize: 30)),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('82,600'),
+                  Text('▼ 82,600', style: TextStyle(
+                    fontSize: 26,
+                    color: Color(0xFF5D99F2)
+                  ),),
                 ],
               ),
               Row(
@@ -150,21 +157,54 @@ class _CreateCardScreen extends State<CardScreen> {
               ),
               Container(
                   height: 250,
-                  child: Card(
-                      elevation: 0.4,
-                      color: Colors.grey,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 18.0),
-                          child: Container()
-                      )
-                  )
+                  child: ChartWidget()
               ),
-              Text('장점: 망해도 국가가 살려줄 국민주'),
-              Text('단점: 내가 벌때 쟤도 범'),
-              Text('더보기')
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Color(0xFFFF8888),
+                    padding: EdgeInsets.symmetric(horizontal: 7),
+                    child: Text('장점', style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white
+                    )),
+                  ),
+                  Padding(padding: EdgeInsets.only(left: 14)),
+                  Text('망해도 국가가 살려줄 국민주',style: TextStyle(
+                    fontSize: 20,)),
+                ],
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Divider(height: 2, color: Colors.grey,),
+              SizedBox(
+                height: 3,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Color(0xFF5D99F2),
+                    padding: EdgeInsets.symmetric(horizontal: 7),
+                    child: Text('단점', style: TextStyle(
+                      fontSize: 20,
+                    color: Colors.white
+                    )),
+                  ),
+                  Padding(padding: EdgeInsets.only(left: 14)),
+                  Text('내가 벌때 쟤도 범',style: TextStyle(
+                    fontSize: 20,)),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text('더보기 >',style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF8C8C8C)
+              ),)
             ],
           ),
           // child: Container(
