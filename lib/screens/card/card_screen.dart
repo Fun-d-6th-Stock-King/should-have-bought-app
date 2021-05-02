ecab868731b43e48e141e7856fcef1e589ac3f56
Soobinnn <isb9082@naver.com>
2021-05-02 19:33:00 +0900

    계산기 버그수정, 랜덤 로딩 구현

--- lib/screens/card/card_screen.dart
+++ lib/screens/card/card_screen.dart
@@ -6,98 +6,46 @@ class CardScreen extends StatefulWidget  {
   @override
   _CreateCardScreen createState() => _CreateCardScreen();
 }
-class _CreateCardScreen extends State<CardScreen> {
-
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  List<SwipeItem> _swipeItems = List<SwipeItem>();
-  MatchEngine _matchEngine;
-  List<String> _names = ["Red", "Blue", "Green", "Yellow", "Orange"];
-  List<Color> _colors = [
-    Colors.red,
-    Colors.blue,
-    Colors.green,
-    Colors.yellow,
-    Colors.orange
-  ];
+class _CreateCardScreen extends State<CardScreen> with SingleTickerProviderStateMixin{
+  AnimationController _controller;
 
   @override
   void initState() {
-    for (int i = 0; i < _names.length; i++) {
-      _swipeItems.add(SwipeItem(
-          content: Content(text: _names[i], color: _colors[i]),
-          likeAction: () {
-            _scaffoldKey.currentState.showSnackBar(SnackBar(
-              content: Text("Liked ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
-          },
-          nopeAction: () {
-            _scaffoldKey.currentState.showSnackBar(SnackBar(
-              content: Text("Nope ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
-          },
-          superlikeAction: () {
-            _scaffoldKey.currentState.showSnackBar(SnackBar(
-              content: Text("Superliked ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
-          }));
-    }
-
-    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 5000),
+      vsync: this,
+    );
     super.initState();
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
-        body: Container(
-            child: Column(children: [
-              Container(
-                height: 300,
-                width: 300,
-                child: SwipeCards(
-                  matchEngine: _matchEngine,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      color: _swipeItems[index].content.color,
-                      child: Text(
-                        _swipeItems[index].content.text,
-                        style: TextStyle(fontSize: 100),
-                      ),
-                    );
-                  },
-                  onStackFinished: () {
-                    _scaffoldKey.currentState.showSnackBar(SnackBar(
-                      content: Text("Stack Finished"),
-                      duration: Duration(milliseconds: 500),
-                    ));
-                  },
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  RaisedButton(
-                      onPressed: () {
-                        _matchEngine.currentItem.nope();
-                      },
-                      child: Text("Nope")),
-                  RaisedButton(
-                      onPressed: () {
-                        _matchEngine.currentItem.superLike();
-                      },
-                      child: Text("Superlike")),
-                  RaisedButton(
-                      onPressed: () {
-                        _matchEngine.currentItem.like();
-                      },
-                      child: Text("Like"))
-                ],
-              )
-            ])));
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RotationTransition(
+              turns: Tween(begin: 0.0, end: 5.0).animate(_controller),
+              child: Image(image: AssetImage('assets/icons/ico_random.png')),
+            ),
+            RaisedButton(
+              child: Text("go"),
+              onPressed: () => _controller.forward(),
+            ),
+            RaisedButton(
+              child: Text("stop"),
+              onPressed: () => _controller.reset(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
