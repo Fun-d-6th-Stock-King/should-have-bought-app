3b67076c1ca37c3226401f9f13f5ef8500941f04
Soobinnn <isb9082@naver.com>
2021-05-06 03:38:55 +0900

    그래프 최고 최저 label 추가

--- lib/widgets/chart/chart_wiget.dart
+++ lib/widgets/chart/chart_wiget.dart
@@ -24,42 +24,85 @@ class _ChartWidgetState extends State<ChartWidget> {
   }
 
   /// Returns the list of spline area series with vertical gradient.
-  List<ChartSeries<_ChartData, String>> _getGradientAreaSeries() {
+  List<LineSeries<_ChartData, num>> _getDataLabelDefaultSeries() {
     final List<_ChartData> chartData = <_ChartData>[
-      _ChartData(x: '1997', y: 22.44),
-      _ChartData(x: '1998', y: 25.18),
-      _ChartData(x: '1999', y: 24.15),
-      _ChartData(x: '2000', y: 25.83),
-      _ChartData(x: '2001', y: 25.69),
-      _ChartData(x: '2002', y: 24.75),
-      _ChartData(x: '2003', y: 27.38),
-      _ChartData(x: '2004', y: 25.31)
+      _ChartData(2005, 10, 28),
+      _ChartData(2006, 24, 44),
+      _ChartData(2007, 36, 48),
+      _ChartData(2008, 38, 50),
+      _ChartData(2009, 54, 66),
+      _ChartData(2010, 57, 78),
+      _ChartData(2011, 70, 84),
+      _ChartData(2012, 40, 28),
+      _ChartData(2013, 44, 44),
+      _ChartData(2014, 36, 48),
+      _ChartData(2015, 38, 50),
+      _ChartData(2016, 54, 66),
+      _ChartData(2017, 57, 78),
+      _ChartData(2018, 70, 84),
+    ];
+    final List<_ChartData> chartData2 = <_ChartData>[
+      _ChartData(2005, 10, 28),
+      _ChartData(2011, 70, 84)
     ];
     final List<Color> color = <Color>[];
-    color.add(const Color(0xFF6A31D5));
-    color.add(const Color(0xFFB650C8));
+    color.add(const Color(0xFFFF6258));
 
     final List<double> stops = <double>[];
     stops.add(0.1);
     stops.add(0.4);
 
-    return <ChartSeries<_ChartData, String>>[
-      SplineAreaSeries<_ChartData, String>(
-        /// To set the gradient colors for series.
-          gradient: const LinearGradient(colors: <Color>[
-            Color.fromRGBO(269, 210, 255, 1),
-            Color.fromRGBO(143, 236, 154, 1)
-          ], stops: <double>[
-            0.2,
-            0.6
-          ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
-          borderWidth: 2,
-          //borderColor: const Color.fromRGBO(0, 156, 144, 1),
-          borderDrawMode: BorderDrawMode.top,
+    return <LineSeries<_ChartData, num>>[
+
+      LineSeries<_ChartData, num>(
+          animationDuration: 0,
+          legendIconType: LegendIconType.rectangle,
           dataSource: chartData,
-          name: 'Country 1',
+          color: const Color(0xFFFF6258),
+          width: 4,
           xValueMapper: (_ChartData sales, _) => sales.x,
-          yValueMapper: (_ChartData sales, _) => sales.y),
+          yValueMapper: (_ChartData sales, _) => sales.y,
+          markerSettings: MarkerSettings(isVisible: false),
+          name: 'Singapore',
+          dataLabelSettings: DataLabelSettings(
+            isVisible: true,
+            useSeriesColor: false,
+            alignment: ChartAlignment.center,
+            labelAlignment: ChartDataLabelAlignment.top,
+            offset: Offset(0, 0),
+          )),
+      // LineSeries<_ChartData, num>(
+      //     animationDuration: 0,
+      //     dataSource: chartData2,
+      //     color: Colors.transparent,
+      //     width: 2,
+      //     name: 'England',
+      //     xValueMapper: (_ChartData sales, _) => sales.x,
+      //     yValueMapper: (_ChartData sales, _) => sales.y,
+      //     markerSettings: MarkerSettings(isVisible: false),
+      //     dataLabelSettings: DataLabelSettings(
+      //       isVisible: true,
+      //       useSeriesColor: false,
+      //       alignment: ChartAlignment.center,
+      //       labelAlignment: ChartDataLabelAlignment.top,
+      //       offset: Offset(0, 0),
+      //     )),
+      // SplineAreaSeries<_ChartData, String>(
+      //   /// To set the gradient colors for series.
+      //     gradient: const LinearGradient(colors: <Color>[
+      //       Color.fromRGBO(269, 210, 255, 1),
+      //       Color.fromRGBO(143, 236, 154, 1)
+      //     ], stops: <double>[
+      //       0.2,
+      //       0.6
+      //     ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+      //     borderWidth: 2,
+      //     //borderColor: const Color.fromRGBO(0, 156, 144, 1),
+      //     borderDrawMode: BorderDrawMode.top,
+      //     dataSource: chartData,
+      //     name: 'Country 1',
+      //     xValueMapper: (_ChartData sales, _) => sales.x,
+      //     yValueMapper: (_ChartData sales, _) => sales.y),
       // SplineAreaSeries<_ChartData, String>(
       //     gradient: const LinearGradient(colors: <Color>[
       //       Color.fromRGBO(140, 108, 245, 1),
@@ -100,19 +143,37 @@ class _ChartWidgetState extends State<ChartWidget> {
       primaryYAxis: NumericAxis(
         isVisible: false,
         minimum: 0,
-        maximum: 28,
+        maximum: 100,
         interval: 4,
-        labelFormat: '{value}%',
+        labelFormat: '{value}',
         axisLine: AxisLine(width: 0),
       ),
+      onDataLabelRender: (DataLabelRenderArgs args) {
+         dataLabel(args);
+      },
       //trackballBehavior: _trackballBehavior,
-      series: _getGradientAreaSeries(),
+      series: _getDataLabelDefaultSeries(),
     );
   }
 }
 
+void dataLabel(DataLabelRenderArgs args) {
+  if(args.pointIndex == 0) {
+     args.textStyle = TextStyle(color:Color(0xFF5D99F2), fontWeight: FontWeight.bold);
+     args.offset = Offset(0,-30);
+     args.text = "최저 ${args.text}";
+   }
+   else if (args.pointIndex == args.dataPoints.length-1) {
+     args.textStyle = TextStyle(color:Color(0xFFFF6258), fontWeight: FontWeight.bold);
+     args.offset = Offset(0,30);
+     args.text = "최고 ${args.text}";
+   } else {
+    args.text = "";
+  }
+}
 class _ChartData {
-  _ChartData({this.x, this.y});
-  final String x;
+  _ChartData(this.x, this.y, this.y2);
+  final double x;
   final double y;
+  final double y2;
 }
