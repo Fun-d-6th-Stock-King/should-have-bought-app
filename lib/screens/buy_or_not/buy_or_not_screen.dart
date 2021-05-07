504f7613715e179053938b3c72817068b334453a
qwa310 <wjdalsdkcla@naver.com>
2021-05-07 20:35:14 +0900

    드립 등록

--- lib/screens/buy_or_not/buy_or_not_screen.dart
+++ lib/screens/buy_or_not/buy_or_not_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class BuyOrNotScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Container(
-        child: Text('BuyOrNotScreen'),
+        child: Column(
+          children: <Widget>[
+            Text(
+              '살까? 말까?',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Container(),
+            Container(),
+            Container(),
+          ],
+        ),
       ),
     );
   }
