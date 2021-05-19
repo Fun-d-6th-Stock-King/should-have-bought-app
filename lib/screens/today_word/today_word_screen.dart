66be2a08300abfad7f6ca67f823cac8b725cdea9
박기현 <keehyun2@gmail.com>
2021-05-19 12:17:51 +0900

    화면 작업중

--- lib/screens/today_word/today_word_screen.dart
+++ lib/screens/today_word/today_word_screen.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:should_have_bought_app/constant.dart';
-import 'package:should_have_bought_app/screens.dart';
-import 'package:should_have_bought_app/widgets/appbar/default_appbar.dart';
-import 'package:should_have_bought_app/widgets/login/login_handler.dart';
-import 'package:should_have_bought_app/widgets/login/login_widget.dart';
-import 'package:should_have_bought_app/widgets/util/loading/loading_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:should_have_bought_app/providers/today_word/today_word_provider.dart';
+import 'package:should_have_bought_app/widgets/appbar/today_word_appbar.dart';
+import 'package:should_have_bought_app/widgets/background/flat_background_frame.dart';
 
 class TodayWordScreen extends StatefulWidget {
   @override
@@ -19,77 +17,171 @@ class _TodayWordScreenState extends State<TodayWordScreen> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+    var parmeters = <String, dynamic>{'order':'LATELY','pageNo':'1','pageSize':'100'};
+    var isEmpty = Provider.of<TodayWordProvider>(context, listen:false).wordItemList.isEmpty ? true : false;
+    if(isEmpty) {
+      Provider.of<TodayWordProvider>(context, listen: false).getWordList(parmeters);
+    }
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DefaultAppBar(context),
-      body: Column(
-        children: [
-          Container(
-            child: Text('TodayWordScreen'),
-          ),
-        ],
+      backgroundColor: Color(0xFFF6F6F6),
+      body: Consumer<TodayWordProvider>(
+          builder: (context, todayWordProvider, child) {
+            var wordItemList = todayWordProvider.wordItemList;
+            return Container(
+              margin:EdgeInsets.only(bottom: 100),
+              child: ListView.builder(
+                  itemCount: wordItemList.length,
+                  itemBuilder: (context, index) {
+                    // if(index == 0) {
+                      return Column(
+                        children: [
+                          FlatBackgroundFrame(
+                              child: HeaderWidget()
+                          ),
+                          SizedBox(height: 37),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text('최신순'),
+                                    Icon(Icons.keyboard_arrow_down_outlined)
+                                  ],
+                                ),
+                                SizedBox(height: 11),
+                                // DripCardWidget(wordItemList[index])
+                              ],
+                            ),
+                          )
+                        ],
+                      );
+                    // }
+                    // return Container(
+                    //   padding: const EdgeInsets.symmetric(horizontal: 16),
+                    //   child: DripCardWidget(wordItemList[index]),
+                    // );
+                  }
+              ),
+            );
+          }
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 100.0),
-        child: FloatingActionButton(
-          backgroundColor: mainColor,
-          onPressed: () => _showCreateWordBottomSheet(context),
-          child: Icon(Icons.add),
-        ),
+      // floatingActionButton: Padding(
+      //   padding: const EdgeInsets.only(bottom: 100.0),
+      //   child: FloatingActionButton(
+      //     backgroundColor: mainColor,
+      //     // onPressed: () => _showCreateWordBottomSheet(context),
+      //     child: Icon(Icons.add),
+      //   ),
+      // ),
+    );
+  }
+}
+
+class HeaderWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          TodayWordAppBar(context),
+          SizedBox(height: 33),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Today's BEST",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(height: 9),
+          BestWordWidget(),
+        ],
       ),
     );
   }
-   // TODO: 등록 버튼 (정민님)
-  void _showCreateWordBottomSheet(BuildContext context) async {
-    showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(8.0),
-          topRight: Radius.circular(8.0),
-        )),
-        builder: (ctx) {
-          return AnimatedPadding(
-              duration: Duration(milliseconds: 150),
-              curve: Curves.easeOut,
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-                height: 300,
-                child: Column(children: [
-                  Container(
-                    child: Text('hello'),
-                  ),
-                  Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    child: CupertinoButton(
-                        color: mainColor,
-                        padding: EdgeInsets.all(0),
-                        child: Text(
-                          '등록하기',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: defaultFontColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        // TODO: 등록로직 필요 (정민님)
-                        onPressed: () => _auth.currentUser == null ? LoginHandler(context) : _registerFlow()),
-                  ),
-                ]),
-              ));
-        });
-  }
-  void _registerFlow() {
+}
 
+class BestWordWidget extends StatefulWidget {
+  @override
+  _CreateBestWordWidgetState createState() => _CreateBestWordWidgetState();
+}
+
+class _CreateBestWordWidgetState extends State<BestWordWidget> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Provider.of<TodayWordProvider>(context, listen: false).getTodayBest();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(left: 20, right: 20, top: 15, bottom: 26),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Consumer<TodayWordProvider>(
+          builder: (context, todayWordProvider, child) {
+        var wordItem = todayWordProvider.todayBest;
+        // EvaluationItem evaluationItem = BestWordProvider.todayBest;
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 13.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    wordItem.wordName ?? '',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        wordItem.wordName ?? '',
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            height: 17 / 12),
+                      ),
+                      Text(
+                        wordItem.wordName ?? '',
+                        style: TextStyle(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w300,
+                            color: Color(0xFF828282)),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 9),
+            Divider(
+              color: Color(0xFF8E8E8E),
+              thickness: 0.6,
+            ),
+            SizedBox(height: 9)
+          ],
+        );
+      }),
+    );
   }
 }
