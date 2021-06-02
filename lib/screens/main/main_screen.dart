cc4108211e817533b676b1c428503ffc1b44c345
Soobinnn <isb9082@naver.com>
2021-06-03 00:37:08 +0900

    UI 완료

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -9,6 +9,7 @@ import 'package:should_have_bought_app/widgets/appbar/default_appbar.dart';
 import 'package:should_have_bought_app/widgets/hey_you_too/hey_you_too.dart';
 import 'package:should_have_bought_app/widgets/main/current_stock_price_widget.dart';
 import 'package:should_have_bought_app/widgets/main/header_widget.dart';
+import 'package:should_have_bought_app/widgets/main/sell_at_this_time_widget.dart';
 import 'package:should_have_bought_app/widgets/main/this_buy_or_not_widget.dart';
 import 'package:should_have_bought_app/widgets/today_word/today_word_best.dart';
 
@@ -38,120 +39,99 @@ class _MainScreenState extends State<MainScreen> {
           await Provider.of<CalculatorProvider>(context, listen: false)
               .getHistory();
         },
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
+          children: [
+            Frame(child: HeaderWidget()),
+            SizedBox(height: 21), // add 계산기
+            Frame(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Frame(child: HeaderWidget()),
-                  SizedBox(height: 21), // add 계산기
-                  Frame(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text("메인편집",
-                            style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                height: 1.5,
-                                color: mainColor)),
-                        //Icon(Icons.fiber_manual_record_rounded, color: Color(0xFF828282))
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  Frame(child: CalculatorWidget()),
-                  SizedBox(height: 50),
-                  Divider(thickness: 7, color: Color(0xFFF2F2F2)),
-                  SizedBox(height: 43),
-                  Frame(
-                    child: MainTitle(title: "그때 살걸... 야, 너두?", more: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HeyToYouMoreScreen()));
-                    }),
-                  ),
-                  SizedBox(height: 16),
-                  HeyYouToo(),
-                  SizedBox(height: 50),
-                  Divider(thickness: 7, color: Color(0xFFF2F2F2)),
-                  SizedBox(height: 43),
-                  Frame(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("현재 주가",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                              height: 1.5,
-                            )),
-                        Container(
-                          padding: EdgeInsets.only(
-                              left: 11, bottom: 2, top: 2, right: 3),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(40.0),
-                            border: Border.all(
-                              width: 1,
-                              color: Color(0xFF828282),
-                            ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("종목선택",
-                                  style: TextStyle(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w400,
-                                      height: 1.5,
-                                      color: Color(0xFF828282))),
-                              Icon(Icons.keyboard_arrow_down_outlined,
-                                  color: Color(0xFF828282))
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  CurrentStockPriceWidget(),
-                  SizedBox(height: 50),
-                  Divider(thickness: 7, color: Color(0xFFF2F2F2)),
-                  SizedBox(height: 43),
-                  Frame(child: MainTitle(title: "오늘의 BEST 단어")),
-                  SizedBox(height: 23),
-                  Frame(child: TodayWordBest()),
-                  SizedBox(height: 51),
-                  Divider(thickness: 7, color: Color(0xFFF2F2F2)),
-                  SizedBox(height: 43),
-                  Frame(child: MainTitle(title: "이거 살까? 말까?", more: () {})),
-                  SizedBox(height: 23),
-                  ThisBuyOrNotWidget(),
-                  Divider(thickness: 7, color: Color(0xFFF2F2F2)),
-                  SizedBox(height: 43),
-                  Frame(child: MainTitle(title: "이때 팔걸")),
-                  SizedBox(
-                      height: 164,
-                      child: Card(
-                        elevation: 0.4,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 18.0),
-                          child: Container(),
-                        ),
+                  Text("메인편집",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          height: 1.5,
+                          color: mainColor)),
+                  //Icon(Icons.fiber_manual_record_rounded, color: Color(0xFF828282))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 7,
+            ),
+            Frame(child: CalculatorWidget()),
+            SizedBox(height: 50),
+            Divider(thickness: 7, color: Color(0xFFF2F2F2)),
+            SizedBox(height: 43),
+            Frame(
+              child: MainTitle(title: "그때 살걸... 야, 너두?", more: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HeyToYouMoreScreen()));
+              }),
+            ),
+            SizedBox(height: 16),
+            HeyYouToo(),
+            SizedBox(height: 50),
+            Divider(thickness: 7, color: Color(0xFFF2F2F2)),
+            SizedBox(height: 43),
+            Frame(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("현재 주가",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        height: 1.5,
                       )),
-                  SizedBox(
-                    height: 66,
-                  ),
-                  SizedBox(
-                    height: 100,
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: 11, bottom: 2, top: 2, right: 3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40.0),
+                      border: Border.all(
+                        width: 1,
+                        color: Color(0xFF828282),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("종목선택",
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w400,
+                                height: 1.5,
+                                color: Color(0xFF828282))),
+                        Icon(Icons.keyboard_arrow_down_outlined,
+                            color: Color(0xFF828282))
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            CurrentStockPriceWidget(),
+            SizedBox(height: 50),
+            Divider(thickness: 7, color: Color(0xFFF2F2F2)),
+            SizedBox(height: 43),
+            Frame(child: MainTitle(title: "오늘의 BEST 단어")),
+            SizedBox(height: 23),
+            Frame(child: TodayWordBest()),
+            SizedBox(height: 51),
+            Divider(thickness: 7, color: Color(0xFFF2F2F2)),
+            SizedBox(height: 43),
+            Frame(child: MainTitle(title: "이거 살까? 말까?", more: () {})),
+            SizedBox(height: 23),
+            ThisBuyOrNotWidget(),
+            Divider(thickness: 7, color: Color(0xFFF2F2F2)),
+            SizedBox(height: 43),
+            Frame(child: MainTitle(title: "이때 팔걸")),
+            SizedBox(height: 23),
+            SellAtThisTimeWidget(),
+            SizedBox(height: 43),
+            SizedBox(height: 100),
+          ],
         ),
       ),
     );
