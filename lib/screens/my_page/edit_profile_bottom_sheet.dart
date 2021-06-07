e36f533eb78eae3da624d5f597ea514ddc1a1b87
Junewoo <korca02220@naver.com>
2021-06-07 18:03:59 +0900

    UI 추가작업 임시 https://github.com/Fun-d-6th-Stock-King/should-have-bought-app/issues/58

--- lib/screens/my_page/edit_profile_bottom_sheet.dart
+++ lib/screens/my_page/edit_profile_bottom_sheet.dart
@@ -1,8 +1,16 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:should_have_bought_app/constant.dart';
+import 'package:should_have_bought_app/widgets/mypage/temp_widget.dart';
 
-class EditProfileBottomSheet extends StatelessWidget {
+class EditProfileBottomSheet extends StatefulWidget {
+  @override
+  _EditProfileBottomSheetState createState() => _EditProfileBottomSheetState();
+}
+
+class _EditProfileBottomSheetState extends State<EditProfileBottomSheet> {
+  int _currentContent = 0;
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -23,87 +31,27 @@ class EditProfileBottomSheet extends StatelessWidget {
             SizedBox(height: 50),
             Container(
               height: 150,
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 150,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: 0.5,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  reverse: false,
-                ),
-                items: <Widget>[
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(150.0),
-                      border: Border.all(
-                        color: mainColor,
-                        width: 3.0,
-                      ),
-                    ),
-                    child: CircleAvatar(
-                      backgroundColor: defaultBackgroundColor,
-                      child: Image(
-                        image: AssetImage('assets/images/normal_chick.png'),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(150.0),
-                      border: Border.all(
-                        color: mainColor,
-                        width: 3.0,
-                      ),
-                    ),
-                    child: CircleAvatar(
-                      backgroundColor: defaultBackgroundColor,
-                      child: Image(
-                        image: AssetImage('assets/images/normal_chick.png'),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(150.0),
-                      border: Border.all(
-                        color: mainColor,
-                        width: 3.0,
-                      ),
-                    ),
-                    child: CircleAvatar(
-                      backgroundColor: defaultBackgroundColor,
-                      child: Image(
-                        image: AssetImage('assets/images/normal_chick.png'),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(150.0),
-                      border: Border.all(
-                        color: mainColor,
-                        width: 3.0,
-                      ),
-                    ),
-                    child: CircleAvatar(
-                      backgroundColor: defaultBackgroundColor,
-                      child: Image(
-                        image: AssetImage('assets/images/normal_chick.png'),
-                      ),
-                    ),
-                  )
-                ],
-              ),
+              child: TempWidget(),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [1, 2, 3, 4].map((url) {
+                int index = [1, 2, 3, 4].indexOf(url);
+                return Container(
+                  width: _currentContent == index ? 32 : 10,
+                  height: 10,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 6.0),
+                  decoration: BoxDecoration(
+                    borderRadius: _currentContent == index
+                        ? BorderRadius.circular(20.0)
+                        : BorderRadius.circular(100.0),
+                    color: _currentContent == index
+                        ? mainColor
+                        : Color(0xFFDFDFDF),
+                  ),
+                );
+              }).toList(),
+            )
           ],
         ),
       ),
